c3e1be1617dfcba153e54176cbc85151c9427008
Preston <preston.dresser16@gmail.ca>
2023-04-10 21:05:16 -0400

    create function prototypes

---
+++ main/inc/env.h
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include "esp_log.h"
+
+#pragma once
+
+typedef struct {
+  double coefficient;
+  double offset;
+  double tco;
+  double y_prev;
+}CurveGenerator;
+
+typedef struct {
+  double attack_tco;
+  double decay_tco;
+  double release_tco;
+  double sustain_level;
+  uint32_t attack_samples;
+  uint32_t decay_samples;
+  uint32_t release_samples;
+}EnvelopeSettings;
+
+typedef enum {
+  ATTACK = 0,
+  DECAY,
+  SUSTAIN,
+  RELEASE
+}EnvelopeState;
+
+typedef struct {
+  EnvelopeSettings settings;
+  CurveGenerator attack;
+  CurveGenerator decay;
+  CurveGenerator release;
+  EnvelopeState state;
+  double amplitude_prev;
+}EnvelopeGenerator;
+
+
+
+void init_envelope(EnvelopeGenerator *env, EnvelopeSettings settings);
+
+double process_curve(CurveGenerator *curve);
+
+double process_envelope(EnvelopeGenerator *env);
+
---
+++ main/src/env.c
@@ -0,0 +1,15 @@
+#include "env.h"
+
+void init_envelope(EnvelopeGenerator *env, EnvelopeSettings settings){
+
+}
+
+double process_curve(CurveGenerator *curve){
+
+  return 0.0;
+}
+
+double process_envelope(EnvelopeGenerator *env){
+
+  return 0.0;
+}
